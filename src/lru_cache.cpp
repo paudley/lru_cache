a1ea521a7bc10ce9824abe1c7d58addf97d3b38e
Patrick <paudley@blackcat.ca>
2017-05-30 11:29:14 -0700

    Add license preamble to source files. This closes #1

--- src/lru_cache.cpp
+++ src/lru_cache.cpp
@@ -1,9 +1,27 @@
 /***************************************************************************
- *   Copyright (C) 2004-2011 by Patrick Audley                             *
+ *   Copyright (C) 2004-2017 by Patrick Audley                             *
  *   paudley@blackcat.ca                                                   *
  *   http://patrickaudley.com                                              *
  *                                                                         *
  ***************************************************************************/
+/*
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Additional licenses are available, see the COPYING file in the parent directory.
+*/
 /**
  * @file lru_cache.cpp Template cache with an LRU removal policy (unit tests)
  * @author Patrick Audley
--- src/lru_cache.h
+++ src/lru_cache.h
@@ -1,14 +1,31 @@
 /***************************************************************************
- *   Copyright (C) 2004-2011 by Patrick Audley                             *
+ *   Copyright (C) 2004-2017 by Patrick Audley                             *
  *   paudley@blackcat.ca                                                   *
  *   http://patrickaudley.com                                              *
  *                                                                         *
  ***************************************************************************/
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Additional licenses are available, see the COPYING file in the parent directory.
+*/
 /**
  * @file lru_cache.h Template cache with an LRU removal policy
  * @author Patrick Audley
  * @version 1.3
- * @date May 2011
+ * @date May 2017
  * @par
  * This cache is thread safe if compiled with _REENTRANT defined.  It
  * uses the BOOST scientific computing library to provide the thread safety
